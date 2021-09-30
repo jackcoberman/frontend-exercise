4765c4f7a6ebda25effa945ea1f290820aa2cc40
Jack <jackcoberman@gmail.com>
2021-09-30 18:54:47 -0400

    Fully functional

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="quiz-component">
-    <h1 class="question">{{questions[currentQuestion].text}}</h1>
-    <v-row>
-    <v-col v-for="option in currentOptions" :key="option">
-      <v-btn class="optn-btn">{{option}}</v-btn>
-    </v-col>
-    </v-row>
-    <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+  <div class="quiz-container" align="center">
+    <div v-if="hasRemainingQuestion" class="quiz-component">
+      <h1 class="question">{{question.text}}</h1>
+      <v-row class="answer-row">
+        <v-col v-for="option in currentOptions" :key="option">
+          <v-btn class="optn-btn" @click="setResponse(option)">{{option}}</v-btn>
+        </v-col>
+      </v-row>
+      <v-row class="next-row">
+        <v-btn class="next-btn" @click="next">Next</v-btn>
+      </v-row>
+    </div>
+    <div v-else>
+      <h1 @click="displayStuff">Results</h1>
+      <v-row class="results-row" v-for="(response,index) in responses" :key="response">
+        <div>{{index+1}}) {{questions[index].text}}</div>
+        <br>
+        <div>{{response}}</div>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -20,23 +32,75 @@ export default {
   },
   data() {
     return {
-      currentQuestion: 0,
-      currentOptions: Array,
-      responses: Array
+      currentIndex: 0,
+      responses: [],
     };
   },
-  created () {
-    this.currentOptions = this.questions[this.currentQuestion].answers;
+  methods: {
+    setResponse (option) {
+      if (Boolean(this.responses[this.currentIndex] == null)) {
+        this.responses.push(option);
+        console.log(this.responses);
+      } else {
+        console.log(option);
+        console.log(this.currentIndex);
+        this.responses[this.currentIndex] = option;
+        console.log(this.responses[this.currentIndex]);
+      }
+    },
+    next () {
+      if (Boolean(this.responses[this.currentIndex] == null)) return;
+      this.currentIndex++;
+    },
+    displayStuff () {
+      console.log(this.responses.length);
+      console.log(this.responses[0]);
+      console.log(this.responses[1]);
+      console.log(this.responses[2]);
+      console.log(this.responses.length); 
+    }
+  },
+  computed: {
+    question () {
+      return this.questions[this.currentIndex];
+    },
+    currentOptions () {
+      return this.questions[this.currentIndex].answers;
+    },
+    canNext () {
+      console.log(Boolean(this.responses[this.currentIndex]));
+      return Boolean(this.responses[this.currentIndex]);
+    },
+    hasRemainingQuestion () {
+      return this.currentIndex < this.questions.length;
+    }
   }
 };
 </script>
 
 <style scoped>
+.quiz-container {
+  max-width: 50%;
+  padding-left: 3em;
+}
 .quiz-component {
-  background-color: aliceblue;
+  padding-top: 10em;
 }
 .question {
-  margin-left: 4em;
   color: rgb(145, 145, 138);
+}
+.answer-row {
+  padding-top: 3em;
+}
+.next-row {
+}
+.results-row {
+  margin-top: 2em;
+}
+.optn-btn {
+  text-transform: none;
+}
+.next-btn {
+  text-transform: none;
 }
 </style>
